ceb664bc23c8addeb91682ca63d0c1c47c68736d
Michael Wautier <wautiermichael@gmail.com>
2020-11-04 16:03:00 +0100

    search form

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,7 +2,11 @@ class FlatsController < ApplicationController
   before_action :set_flat, only: %i[show edit update destroy]
 
   def index
-    @flats = Flat.all
+    if params[:query]
+      @flats = Flat.where("name LIKE '%#{params[:query]}%'")
+    else
+      @flats = Flat.all
+    end
   end
 
   def new
