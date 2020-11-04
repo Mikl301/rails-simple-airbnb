6fbb123307841092866ad4872daab9d6e3ec91e8
Michael Wautier <wautiermichael@gmail.com>
2020-11-04 15:56:22 +0100

    all actions completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating flats...'
+10.times do
+  random_house_picture = "https://source.unsplash.com/#{rand(1500..1600)}x#{rand(850..950)}/?house"
+  Flat.create!(
+    name: "#{Faker::JapaneseMedia::OnePiece.island} House",
+    address: Faker::JapaneseMedia::OnePiece.location,
+    description: Faker::Movies::HarryPotter.quote,
+    picture_url: random_house_picture,
+    price_per_night: rand(50..100),
+    number_of_guests: rand(2..6)
+  )
+end
+puts 'Finished!'
